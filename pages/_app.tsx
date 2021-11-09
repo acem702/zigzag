fe9d88533e47af5f45e736cf47907cdfe0f43265
julesfll <jfl4kb@virginia.edu>
2021-11-08 22:17:56 -0500

    Revert "Add material UI"
    This reverts commit 7b6f0efaf64e10296e294a52a968595736e4a6ff.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,8 @@
-import Head from "next/head";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material";
-import { CacheProvider } from "@emotion/react";
-import createEmotionCache from "../styles/createEmotionCache";
-import type { AppProps } from "next/app";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-import theme from "../styles/theme";
-
-const clientSideEmotionCache = createEmotionCache();
-
-function MyApp({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}: AppProps) {
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Zig Zag</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
